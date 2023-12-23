5427aa0a731c870a945a4e7a620fcf6a7cd6e1a3
farouqdaif1 <daiffarouq@gmail.com>
2023-12-23 16:40:19 +0200

    Fix  styles

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function addEventListenerToInput(inputElement) {
     inputElement.addEventListener("keydown", function (event) {
         let regex1 = /^\/1.*/;
         let regex2 = /^\/1$/;
-        if ((event.key === "Enter" || event.keyCode === 13) && inputElement.placeholder === "Enter text here" && regex2.test(inputValue)) {
+        if ((event.key === "Enter" || event.keyCode === 13) && inputElement.placeholder === "Type / for blocks , @ to link docs or people" && regex2.test(inputValue)) {
             inputElement.value = "";
             inputElement.placeholder = "Heading 1";
             inputElement.classList.add("heading-1");
@@ -29,15 +29,15 @@ function addEventListenerToInput(inputElement) {
             header.innerHTML = ` <i class="fa-solid fa-bars bars"></i>&nbsp;&nbsp;${inputElement.value}`;
             inputElement.parentNode.insertBefore(header, inputElement.nextSibling);
             generateNewNextInput(header);
-        } else if ((event.key === "Enter" || event.keyCode === 13) && inputElement.placeholder === "Enter text here" && !regex1.test(inputValue)) {
+        } else if ((event.key === "Enter" || event.keyCode === 13) && inputElement.placeholder === "Type / for blocks , @ to link docs or people" && !regex1.test(inputValue)) {
             inputElement.value = this.value;
+            inputElement.style.fontColor = "#4d5562";
             generateNewNextInput(inputElement);
-        } else if ((event.key === "Enter" || event.keyCode === 13) && inputElement.placeholder === "Enter text here" && regex1.test(inputValue)) {
+        } else if ((event.key === "Enter" || event.keyCode === 13) && inputElement.placeholder === "Type / for blocks , @ to link docs or people" && regex1.test(inputValue)) {
             inputElement.value = this.value;
             let string = inputElement.value.slice(2)
             inputElement.style.display = "none";
             let header = document.createElement("h1");
-
             header.innerHTML = ` <i class="fa-solid fa-bars bars"></i>&nbsp;&nbsp;${string}`;
             inputElement.parentNode.insertBefore(header, inputElement.nextSibling);
             generateNewNextInput(header);
@@ -101,7 +101,7 @@ function generateNewNextInput(header) {
         let textInput = document.createElement("input");
         textInput.type = "text";
         textInput.name = "text-input";
-        textInput.placeholder = "Enter text here";
+        textInput.placeholder = "Type / for blocks , @ to link docs or people";
         header.parentNode.insertBefore(textInput, header.nextSibling);
         textInput.focus();
         textInput.setAttribute('autocomplete', 'off');
