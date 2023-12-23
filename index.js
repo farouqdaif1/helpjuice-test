8f7cf29f6cc9b4e9a65e6ee8bf46de522d12ca9c
farouqdaif1 <daiffarouq@gmail.com>
2023-12-23 17:34:41 +0200

    Fix  styles for font in tex

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ function addEventListenerToInput(inputElement) {
     let inputValue;
     inputElement.addEventListener("input", function () {
         inputValue = inputElement.value;
+        if (inputElement.placeholder === "Heading 1") {
+            inputElement.style.color = "#212936";
+        } else {
+            inputElement.style.color = "#4d5562";
+        }
         generateDropdownOptions(inputElement, inputValue);
     });
     inputElement.addEventListener("keydown", function (event) {
@@ -14,7 +19,6 @@ function addEventListenerToInput(inputElement) {
             inputElement.value = "";
             inputElement.placeholder = "Heading 1";
             inputElement.classList.add("heading-1");
-            // inputElement.style.hight = "2em";
             inputElement.style.fontSize = "3em";
             inputElement.focus();
             let nextSibling = inputElement.nextElementSibling;
@@ -26,7 +30,7 @@ function addEventListenerToInput(inputElement) {
             inputElement.value = this.value;
             inputElement.style.display = "none";
             let header = document.createElement("h1");
-            header.innerHTML = ` <i class="fa-solid fa-bars bars"></i>&nbsp;&nbsp;${inputElement.value}`;
+            header.innerHTML = inputElement.value;
             inputElement.parentNode.insertBefore(header, inputElement.nextSibling);
             generateNewNextInput(header);
         } else if ((event.key === "Enter" || event.keyCode === 13) && inputElement.placeholder === "Type / for blocks , @ to link docs or people" && !regex1.test(inputValue)) {
@@ -38,7 +42,7 @@ function addEventListenerToInput(inputElement) {
             let string = inputElement.value.slice(2)
             inputElement.style.display = "none";
             let header = document.createElement("h1");
-            header.innerHTML = ` <i class="fa-solid fa-bars bars"></i>&nbsp;&nbsp;${string}`;
+            header.innerHTML = string;
             inputElement.parentNode.insertBefore(header, inputElement.nextSibling);
             generateNewNextInput(header);
         }
@@ -86,6 +90,8 @@ function generateDropdownOptions(inputElement, inputValue) {
         optionElement.addEventListener("click", function () {
             inputElement.value = "";
             inputElement.placeholder = "Heading 1";
+            // inputElement.style.fontColor = "#212936";
+            inputElement.style.fontSize = "3em";
             inputElement.classList.add("heading-1");
             inputElement.focus();
             let nextSibling = inputElement.nextElementSibling;
