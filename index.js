8b5b29cb6546f7f14093673102f3fa90497bd303
farouqdaif1 <daiffarouq@gmail.com>
2023-12-23 06:55:38 +0200

    Fix Add function

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ function addEventListenerToInput(inputElement) {
         } else if ((event.key === "Enter" || event.keyCode === 13) && inputElement.placeholder === "Enter text here" && !regex1.test(inputValue)) {
             inputElement.value = this.value;
             generateNewNextInput(inputElement);
-            console.log("third if");
         } else if ((event.key === "Enter" || event.keyCode === 13) && inputElement.placeholder === "Enter text here" && regex1.test(inputValue)) {
             inputElement.value = this.value;
             let string = inputElement.value.slice(2)
@@ -47,34 +46,36 @@ function addEventListenerToInput(inputElement) {
 
 function generateDropdownOptions(inputElement, inputValue) {
     let regex2 = /^\/1$/;
-    if (regex2.test(inputValue)) {
-        console.log(inputValue);
-        let dropdownElement = document.createElement("div");
-        dropdownElement.className = "dropdown";
-        inputElement.parentNode.insertBefore(dropdownElement, inputElement.nextSibling);
-        dropdownElement.innerHTML = "";
-        let option1 = document.createElement("div");
-        option1.className = "option";   
-        option1.innerHTML = `
-            <h4>Add blocks</h4>
-            <p class="after-h4">Keep typing to filter or escape to exit</p>
-            <p>Filtering Keyword <span>1<span></p>
-            <div>
-            <i class="fa-solid fa-t"></i>
-            <p>
-            <strong>Heading 1</strong>
-            </p>
-            <p>ShortCut: type >># + space </p>
-            </div>
-        `;
-        dropdownElement.appendChild(option1);
-        dropdownElement.style.display = "block";
-    } else {
-        let nextSibling = inputElement.nextElementSibling;
-        if (nextSibling) {
-            nextSibling.remove();
+    if (inputElement.placeholder !== "Heading 1") {
+        if (regex2.test(inputValue)) {
+            let dropdownElement = document.createElement("div");
+            dropdownElement.className = "dropdown";
+            inputElement.parentNode.insertBefore(dropdownElement, inputElement.nextSibling);
+            dropdownElement.innerHTML = "";
+            let option1 = document.createElement("div");
+            option1.className = "option";
+            option1.innerHTML = `
+                <h4>Add blocks</h4>
+                <p class="after-h4">Keep typing to filter or escape to exit</p>
+                <p>Filtering Keyword <span>1<span></p>
+                <div>
+                <i class="fa-solid fa-t"></i>
+                <p>
+                <strong>Heading 1</strong>
+                </p>
+                <p>ShortCut: type >># + space </p>
+                </div>
+            `;
+            dropdownElement.appendChild(option1);
+            dropdownElement.style.display = "block";
+        } else {
+            let nextSibling = inputElement.nextElementSibling;
+            if (nextSibling) {
+                nextSibling.remove();
+            }
         }
     }
+
 
 }
 function generateNewNextInput(header) {
