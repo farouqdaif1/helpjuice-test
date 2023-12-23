0daa951319b7a544c234974b8d42bb26500797bd
farouqdaif1 <daiffarouq@gmail.com>
2023-12-23 06:13:45 +0200

    Add styles

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let inputElOne = document.getElementById('first-input');
 addEventListenerToInput(inputElOne);
+
 function addEventListenerToInput(inputElement) {
     let inputValue;
     inputElement.addEventListener("input", function () {
@@ -7,12 +8,13 @@ function addEventListenerToInput(inputElement) {
         generateDropdownOptions(inputElement, inputValue);
     });
     inputElement.addEventListener("keydown", function (event) {
-        let nextSibling = inputElement.nextElementSibling;
         let regex1 = /^\/1.*/;
         let regex2 = /^\/1$/;
         if ((event.key === "Enter" || event.keyCode === 13) && inputElement.placeholder === "Enter text here" && regex2.test(inputValue)) {
             inputElement.value = "";
             inputElement.placeholder = "Heading 1";
+            let nextSibling = inputElement.nextElementSibling;
+
             if (nextSibling) {
                 nextSibling.remove();
             }
@@ -20,31 +22,22 @@ function addEventListenerToInput(inputElement) {
             inputElement.value = this.value;
             inputElement.style.display = "none";
             let header = document.createElement("h1");
-            header.innerHTML = inputElement.value;
+            header.innerHTML = ` <i class="fa-solid fa-bars bars"></i>&nbsp;&nbsp;${inputElement.value}`;
             inputElement.parentNode.insertBefore(header, inputElement.nextSibling);
-            header.addEventListener("click", function () {
-                header.style.display = "none";
-                inputElement.style.display = "block";
-                inputElement.focus();
-            })
             generateNewNextInput(header);
-
         } else if ((event.key === "Enter" || event.keyCode === 13) && inputElement.placeholder === "Enter text here" && !regex1.test(inputValue)) {
             inputElement.value = this.value;
             generateNewNextInput(inputElement);
+            console.log("third if");
         } else if ((event.key === "Enter" || event.keyCode === 13) && inputElement.placeholder === "Enter text here" && regex1.test(inputValue)) {
             inputElement.value = this.value;
             let string = inputElement.value.slice(2)
             inputElement.style.display = "none";
             let header = document.createElement("h1");
-            header.innerHTML = string;
+
+            header.innerHTML = ` <i class="fa-solid fa-bars bars"></i>&nbsp;&nbsp;${string}`;
             inputElement.parentNode.insertBefore(header, inputElement.nextSibling);
             generateNewNextInput(header);
-            header.addEventListener("click", function () {
-                header.style.display = "none";
-                inputElement.style.display = "block";
-                inputElement.focus();
-            })
         }
     });
 
@@ -53,7 +46,6 @@ function addEventListenerToInput(inputElement) {
 }
 
 function generateDropdownOptions(inputElement, inputValue) {
-    let regex1 = /^\/1.*/;
     let regex2 = /^\/1$/;
     if (regex2.test(inputValue)) {
         console.log(inputValue);
@@ -80,18 +72,18 @@ function generateDropdownOptions(inputElement, inputValue) {
         if (nextSibling) {
             nextSibling.remove();
         }
-
     }
 
 }
 function generateNewNextInput(header) {
-    let textInput = document.createElement("input");
-    textInput.type = "text";
-    textInput.name = "text-input";
-    textInput.placeholder = "Enter text here";
-    header.parentNode.insertBefore(textInput, header.nextSibling);
-    textInput.focus();
-
-    addEventListenerToInput(textInput);
-
+    if (!header.nextElementSibling) {
+        let textInput = document.createElement("input");
+        textInput.type = "text";
+        textInput.name = "text-input";
+        textInput.placeholder = "Enter text here";
+        header.parentNode.insertBefore(textInput, header.nextSibling);
+        textInput.focus();
+        textInput.setAttribute('autocomplete', 'off');
+        addEventListenerToInput(textInput);
+    }
 }
